7f91ed4b5b338eafd35239ad35fde43a6bab5f25
Josh Sherman <josh@gravityblvd.com>
2007-10-06 20:38:01 +0000

    Added config files named for the full server name, updated the loader accordingly.
    git-svn-id: http://svn.cleancode.org/svn/pickles@2 4d10bc64-7434-11dc-a737-d2d0f8310089

--- jLib.php
+++ jLib.php
@@ -4,12 +4,6 @@ function __autoload($class) {
 	require_once "classes/{$class}.php";
 }
 
-$server = explode('.', $_SERVER["SERVER_NAME"]);
-$site   = $server[count($server) - 2];
-
-// @debug
-$site = 'meatronic';
-
-Config::load($site);
+Config::load($_SERVER['SERVER_NAME']);
 
 ?>
