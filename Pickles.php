2b9ad5ef77d56cd79c9bf859cd8c40bf77fc210f
Josh Sherman <josh@gravityblvd.com>
2008-10-09 15:51:55 +0000

    Rearranged some directories so the root isn't so cluttered.  Added the copyright notice header to Pickles.php
    git-svn-id: http://svn.cleancode.org/svn/pickles@62 4d10bc64-7434-11dc-a737-d2d0f8310089

--- Pickles.php
+++ Pickles.php
@@ -8,10 +8,26 @@
  * is established as well as the path that Smarty will use to store the
  * compiled pages.
  *
+ * PICKLES is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * PICKLES is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with PICKLES.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author    Joshua John Sherman <josh@phpwithpickles.org>
+ * @copyright Copyright 2007, 2008 Joshua John Sherman
+ * @link      http://phpwithpickles.org
+ * @license   http://www.gnu.org/copyleft/lesser.html
  * @package   PICKLES
- * @author    Joshua Sherman <josh@phpwithpickles.org>
- * @copyright 2007-2008 Joshua Sherman
- * @usage     require_once 'Pickles.php';
+ * @usage     <code>require_once 'Pickles.php'</code>;
  */
 
 /**
@@ -34,8 +50,8 @@ Config::load(SITE_PATH . '../config.xml');
 /**
  * Magic function to automatically load classes
  *
- * Determines if the system needs to load a PICKLES core class or a PICKLES
- * shared model (not to be confused with site level models).
+ * Determines if the system needs to load a PICKLES core class or
+ * a PICKLES shared model (not to be confused with site level models).
  *
  * @param  string $class Name of the class to be loaded
  * @return boolean Return value of require_once() or false (default)
