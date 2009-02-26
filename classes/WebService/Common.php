6958127c483ed592af3ec2c1f652e672b635b999
Josh Sherman <josh@gravityblvd.com>
2009-02-26 01:07:01 +0000

    Restructuring.
    git-svn-id: http://svn.cleancode.org/svn/pickles@94 4d10bc64-7434-11dc-a737-d2d0f8310089

---
+++ classes/WebService/Common.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Common Web Service Class File for PICKLES
+ *
+ * PICKLES is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * PICKLES is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with PICKLES.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author    Joshua John Sherman <josh@phpwithpickles.org>
+ * @copyright Copyright 2009 Joshua John Sherman
+ * @link      http://phpwithpickles.org
+ * @license   http://www.gnu.org/copyleft/lesser.html
+ * @package   PICKLES
+ */
+
+/**
+ * Common Web Service Class
+ *
+ * This is the class that each gateway class should be extending from.
+ */
+abstract class WebService_Common extends Object {
+	
+	/**
+	 * Constructor
+	 *
+	 * Runs the parent's constructor and adds the module to the object.
+	 */
+	public function __construct(Config $config, Error $error) {
+		parent::__construct();
+
+		$this->config = $config;
+		$this->error  = $error;
+	}
+
+	/**
+	 * Abstract processing function that is overloaded within the loaded gateway
+	 */
+	public abstract function process();
+}
+
+?>
