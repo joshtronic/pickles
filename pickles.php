52ee04a9ee3820d327890f93d4f6bdfdd65b7246
Josh Sherman <josh@gravityblvd.com>
2010-03-09 21:43:24 -0500

    Updated header and remove LGPL
     * Licensing has changed to GPLv3

--- pickles.php
+++ pickles.php
@@ -1,38 +1,22 @@
 <?php
 
 /**
- * PICKLES core include file
+ * Core PICKLES Include File
  *
  * This is the file that you include on the page you're instantiating the
- * controller from (typically index.php).  The path to the PICKLES code base
- * is established as well as the path that Smarty will use to store the
- * compiled pages.
+ * controller from (typically index.php).  The path to the PICKLES code
+ * base is established as well as the path that Smarty will use to store
+ * the compiled pages.
  *
- * PICKLES is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * PICKLES is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with PICKLES.  If not, see
- * <http://www.gnu.org/licenses/>.
- *
- * @author    Joshua John Sherman <josh@phpwithpickles.org>
- * @copyright Copyright 2007, 2008, 2009 Joshua John Sherman
+ * @author    Josh Sherman <josh@phpwithpickles.org>
+ * @copyright Copyright 2007-2010, Gravity Boulevard, LLC
  * @link      http://phpwithpickles.org
- * @license   http://www.gnu.org/copyleft/lesser.html
- * @package   PICKLES
- * @usage     <code>require_once 'Pickles.php'</code>;
+ * @license   http://www.gnu.org/licenses/gpl.html
+ * @package   pickles
+ * @usage     <code>require_once 'pickles.php';</code>;
  */
-
-/**
- * @todo Allow users to override the timezone from their configuration file.
- */
+ 
+// @todo Allow users to override the timezone from their configuration file.
 // Sets the timezone to avoid Smarty warnings
 if (ini_get('date.timezone') == '')
 {
