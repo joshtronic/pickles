31624b5d2c0195fed0147922b03f14d49d495ef2
Josh Sherman <josh@gravityblvd.com>
2010-03-12 23:28:52 -0500

    Updated docs.
     * Added README to advise on how to generate docs
    
     * Moved generate file to a sub directory under scripts.

---
+++ scripts/docs/generate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+phpdoc -o HTML:frames:l0l33t -d ../../ -t ../../docs -dn Miscellaneous -ct usage -ti "PHP Interface Collection of Killer Libraries to Enhance Stuff"
+
+#-po PICKLES 
+#-dc PICKLES
--- scripts/generate_docs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-phpdoc -o HTML:frames:l0l33t -d ../ -t ../docs -dn Miscellaneous -po PICKLES -ct usage -s
-
-#-dc PICKLES
-#-ti "PHP Interface Collection of Killer Libraries to Enhance Stuff"
