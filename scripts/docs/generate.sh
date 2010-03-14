6434c2a6c8cd282a8172c3c342ebfa2e73b48f70
Josh Sherman <josh@gravityblvd.com>
2010-03-14 11:10:21 -0400

    Added TODO file
     * Updated documentation generation script to include the TODO file.

--- scripts/docs/generate.sh
+++ scripts/docs/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-phpdoc -d ../../ -t ../../docs -ct usage -ti "PHP Interface Collection of Killer Libraries to Enhance Stuff" -ric README,INSTALL,COPYING -po PICKLES -s
+phpdoc -d ../../ -t ../../docs -ct usage -ti "PHP Interface Collection of Killer Libraries to Enhance Stuff" -ric README,INSTALL,COPYING,TODO -po PICKLES -s
