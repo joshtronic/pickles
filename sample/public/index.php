f4cf8d5a9a07403664afb53d2af239eb3ec04593
Josh Sherman <josh@gravityblvd.com>
2010-03-27 15:24:46 -0400

    Added a sample Apache config.

--- sample/public/index.php
+++ sample/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-ini_set('include_path', ini_get('include_path') . ':' . getcwd() . '../');
+// ini_set('include_path', ini_get('include_path') . ':/path/to/pickles');
 
 require_once 'pickles.php';
 new Controller();
