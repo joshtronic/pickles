ef5b8d2ef173374c20a87f2be5c0f4d94bf46daf
Josh Sherman <josh@gravityblvd.com>
2010-09-12 22:13:00 -0400

    Updated docs, also updated doc generation script.

--- scripts/docs/generate.sh
+++ scripts/docs/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+rm ../../docs -rf
 phpdoc -d ../../ -t ../../docs -ct usage -ti "PHP Interface Collection of Killer Libraries to Enhance Stuff" -ric README,INSTALL,COPYING,TODO -po PICKLES -s
