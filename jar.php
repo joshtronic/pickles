b7fd659b3c1be5c8dd9871840542d42b12a3add1
Josh Sherman <josh@gravityblvd.com>
2011-08-28 17:44:30 -0400

    Apparently the Google Profanity class wasn't in the jar.

--- jar.php
+++ jar.php
@@ -41,6 +41,50 @@ abstract class API_Common extends Object
 }
 
 /**
+ * Google Profanity Class File for PICKLES
+ *
+ * PHP version 5
+ *
+ * Licensed under The MIT License
+ * Redistribution of these files must retain the above copyright notice.
+ *
+ * @author    Josh Sherman <josh@gravityblvd.com>
+ * @copyright Copyright 2007-2011, Josh Sherman
+ * @license   http://www.opensource.org/licenses/mit-license.html
+ * @package   PICKLES
+ * @link      http://p.ickl.es
+ */
+
+/**
+ * Google Profanity API Interface 
+ */
+class API_Google_Profanity extends API_Common
+{
+	/**
+	 * Check
+	 *
+	 * Checks if a word is considered profanity.
+	 *
+	 * @usage API_Google_Profanity::check('fuck'); // returns true
+	 * @param string $word word to check
+	 * @return boolean whether or not the word is profanity
+	 */
+	public static function check($word)
+	{
+		$response = json_decode(file_get_contents('http://www.wdyl.com/profanity?q=' . $word), true);
+
+		if ($response == null || !isset($response['response']) || !in_array($response['response'], array('true', 'false')))
+		{
+			throw new Exception('Invalid response from API.');
+		}
+		else
+		{
+			return $response['response'] == 'true';
+		}
+	}
+}
+
+/**
  * Tinychat Class File for PICKLES
  *
  * PHP version 5
