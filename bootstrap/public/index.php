7644edb8b4d5d9502137b12c08873da42822dd66
Josh Sherman <josh@gravityblvd.com>
2012-10-10 19:37:24 -0400

    Started laying out the bootstrap files

---
+++ bootstrap/public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Where dem PICKLES at?!
+set_include_path(get_include_path() . PATH_SEPARATOR . '/usr/share/pickles');
+
+// Give'm to me!
+require_once 'pickles.php';
+
+// OM NOM NOM NOM!!~!
+new Controller();
+
+?>
