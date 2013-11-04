d96181c60f5ecfb647d1f5a76774b1b9968d489d
Josh Sherman <josh@gravityblvd.com>
2013-11-04 17:36:04 -0500

    Q4 2013, seemed like a good time to update the copyright notices

--- classes/Model.php
+++ classes/Model.php
@@ -8,8 +8,8 @@
  * Licensed under The MIT License
  * Redistribution of these files must retain the above copyright notice.
  *
- * @author    Josh Sherman <pickles@joshtronic.com>
- * @copyright Copyright 2007-2013, Josh Sherman
+ * @author    Joshua Sherman <pickles@joshtronic.com>
+ * @copyright Copyright 2007-2013, Joshua Sherman
  * @license   http://www.opensource.org/licenses/mit-license.html
  * @package   PICKLES
  * @link      https://github.com/joshtronic/pickles
