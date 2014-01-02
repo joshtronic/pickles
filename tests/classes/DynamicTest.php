3fee938c2a04fb2ab797b0b5f5ebf5fb3a6808a3
Joshua Sherman <josh@gravityblvd.com>
2014-01-02 01:17:25 -0500

    Stubbed out and wrote some tests
    Will code tests for the stub files in the AM.

---
+++ tests/classes/DynamicTest.php
@@ -0,0 +1,81 @@
+<?php
+
+class DynamicTest extends PHPUnit_Framework_TestCase
+{
+	public function testReferenceWithoutFailover()
+	{
+
+	}
+
+	public function testReferenceWithFailover()
+	{
+
+	}
+
+	public function testCSSWrongExtension()
+	{
+
+	}
+
+	public function testCSSErrorLESS()
+	{
+
+	}
+
+	public function testCSSErrorSASS()
+	{
+
+	}
+
+	public function testCSS()
+	{
+
+	}
+
+	public function testCSSWithLESS()
+	{
+
+	}
+
+	public function testCSSWithSASS()
+	{
+
+	}
+
+	public function testCSSUnableToMinify()
+	{
+
+	}
+
+	public function testCSSDoesNotExist()
+	{
+
+	}
+
+	public function testJSWrongExtension()
+	{
+
+	}
+
+	public function testJSUnableToMinify()
+	{
+
+	}
+
+	public function testJSDoesNotExist()
+	{
+
+	}
+
+	public function testJSInvalidLevel()
+	{
+
+	}
+
+	public function testJS()
+	{
+
+	}
+}
+
+?>
---
+++ tests/classes/LogTest.php
@@ -0,0 +1,41 @@
+<?php
+
+class LogTest extends PHPUnit_Framework_TestCase
+{
+	public function testInformation()
+	{
+
+	}
+
+	public function testWarning()
+	{
+
+	}
+
+	public function testError()
+	{
+
+	}
+
+	public function testSlowQuery()
+	{
+
+	}
+
+	public function testTransaction()
+	{
+
+	}
+
+	public function testPHPError()
+	{
+
+	}
+
+	public function testQuery()
+	{
+
+	}
+}
+
+?>
