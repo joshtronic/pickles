161d0e50515f7325278cb0cbbadb1e590c7edfb7
Joshua Sherman <josh@gravityblvd.com>
2014-01-04 11:33:13 -0500

    Stubbed out Profiler tests

---
+++ tests/classes/ProfilerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+class ProfilerTest extends PHPUnit_Framework_TestCase
+{
+	public function testEnabled()
+	{
+		// $this->assertTrue(Profiler::enabled());
+	}
+
+	public function testDisabled()
+	{
+		// $this->assertFalse(Profiler::enabled());
+	}
+
+	public function testEnabledType()
+	{
+
+	}
+
+	public function testDisabledType()
+	{
+
+	}
+
+	public function testLogArray()
+	{
+
+	}
+
+
+	public function testLogObject()
+	{
+
+	}
+
+	public function testLogTimer()
+	{
+
+	}
+
+	public function testLogString()
+	{
+
+	}
+
+	public function testLogQuery()
+	{
+
+	}
+
+	public function testTimerStart()
+	{
+
+	}
+
+	public function testTimerEnd()
+	{
+
+	}
+
+	public function testReport()
+	{
+
+	}
+}
+
+?>
