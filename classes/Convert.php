c865e74ebd6b36fab1d8a54e9482714f73b599e2
Joshua Sherman <josh@gravityblvd.com>
2014-01-26 21:35:56 -0500

    Updated headers and dropped tests
    The tests weren't complete nor were they being used so I dropped them. Since
    this branch is still actively being maintained as it's the PHP 5.3 compatible
    stuff I figured I should update the headers to have the current year and
    explicitly show that it's PHP 5.3+ and not just PHP 5.

--- classes/Convert.php
+++ classes/Convert.php
@@ -3,13 +3,13 @@
 /**
  * Converter
  *
- * PHP version 5
+ * PHP version 5.3+
  *
  * Licensed under The MIT License
  * Redistribution of these files must retain the above copyright notice.
  *
- * @author    Joshua Sherman <pickles@joshtronic.com>
- * @copyright Copyright 2007-2013, Joshua Sherman
+ * @author    Joshua Sherman <josh@gravityblvd.com>
+ * @copyright Copyright 2007-2014, Joshua Sherman
  * @license   http://www.opensource.org/licenses/mit-license.html
  * @package   PICKLES
  * @link      https://github.com/joshtronic/pickles
