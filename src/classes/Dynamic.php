86ef824455ec40f15659d216ce59ebc6dc8c57ef
Josh Sherman <josh@gravityblvd.com>
2014-02-20 15:55:02 -0500

    Only compile LESS or SASS if minify flag is true
    Probably need to rename the minify flag since it now also represents compiling the CSS. Better yet, probably better to eliminate it entirely and make it an assumption that you should only be compiling when on the "local" environment or when the minified file is completely missing.

--- src/classes/Dynamic.php
+++ src/classes/Dynamic.php
@@ -150,31 +150,31 @@ class Dynamic extends Object
 		{
 			$reference = $original_reference;
 
-			// Compiles LESS & SASS to CSS before minifying
-			if ($less || $sass)
-			{
-				$compiled_filename = str_replace('.min', '', $minified_filename);
-
-				if ($less)
-				{
-					$less = new lessc();
-					$less->compileFile($original_filename, $compiled_filename);
-				}
-				elseif ($sass)
-				{
-					$scss = new scssc();
-
-					file_put_contents(
-						$compiled_filename,
-						$scss->compile(file_get_contents($original_filename))
-					);
-				}
-
-				$original_filename = $compiled_filename;
-			}
-
 			if ($this->config->pickles['minify'] === true)
 			{
+				// Compiles LESS & SASS to CSS before minifying
+				if ($less || $sass)
+				{
+					$compiled_filename = str_replace('.min', '', $minified_filename);
+
+					if ($less)
+					{
+						$less = new lessc();
+						$less->compileFile($original_filename, $compiled_filename);
+					}
+					elseif ($sass)
+					{
+						$scss = new scssc();
+
+						file_put_contents(
+							$compiled_filename,
+							$scss->compile(file_get_contents($original_filename))
+						);
+					}
+
+					$original_filename = $compiled_filename;
+				}
+
 				// Minifies CSS with a few basic character replacements.
 				$stylesheet = file_get_contents($original_filename);
 				$stylesheet = str_replace(
