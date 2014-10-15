f235f4a520f787b8905a5d5a2d50387d41cb4342
Josh Sherman <josh@gravityblvd.com>
2014-10-15 07:48:34 -0400

    Tweaking the schema

--- src/OAuth2/AccessTokenStorage.php
+++ src/OAuth2/AccessTokenStorage.php
@@ -11,10 +11,10 @@ class AccessTokenStorage extends StorageAdapter implements AccessTokenInterface
 {
     public function get($token)
     {
-        $sql = 'SELECT oauth_session_access_tokens.*'
-             . ' FROM oauth_session_access_tokens'
+        $sql = 'SELECT oauth_access_tokens.*'
+             . ' FROM oauth_access_tokens'
              . ' WHERE access_token = ?'
-             . ' AND access_token_expires >= ?;';
+             . ' AND expires_at >= ?;';
 
         $results = $this->db->fetch($sql, [$token, time()]);
 
@@ -22,7 +22,7 @@ class AccessTokenStorage extends StorageAdapter implements AccessTokenInterface
         {
             return (new AccessTokenEntity($this->server))
                 ->setId($results[0]['access_token'])
-                ->setExpireTime($results[0]['access_token_expires']);
+                ->setExpireTime($results[0]['expires_at']);
         }
 
         return null;
@@ -31,10 +31,10 @@ class AccessTokenStorage extends StorageAdapter implements AccessTokenInterface
     public function getScopes(AbstractTokenEntity $token)
     {
         $sql = 'SELECT oauth_scopes.id, oauth_scopes.description'
-             . ' FROM oauth_session_token_scopes'
+             . ' FROM oauth_access_token_scopes'
              . ' INNER JOIN oauth_scopes'
-             . ' ON oauth_session_token_scopes.scope_id = oauth_scopes.id'
-             . ' WHERE oauth_session_token_scopes.session_access_token_id = ?;';
+             . ' ON oauth_access_token_scopes.scope_id = oauth_scopes.id'
+             . ' WHERE oauth_access_token_scopes.access_token_id = ?;';
 
         $results  = $this->db->fetch($sql, [$token->getId()]);
         $response = [];
@@ -55,8 +55,8 @@ class AccessTokenStorage extends StorageAdapter implements AccessTokenInterface
 
     public function create($token, $expiration, $session_id)
     {
-        $sql = 'INSERT INTO oauth_session_access_tokens'
-             . ' (access_token, session_id, access_token_expires)'
+        $sql = 'INSERT INTO oauth_access_tokens'
+             . ' (access_token, session_id, expires_at)'
              . ' VALUES'
              . ' (?, ?, ?);';
 
@@ -65,7 +65,7 @@ class AccessTokenStorage extends StorageAdapter implements AccessTokenInterface
 
     public function associateScope(AbstractTokenEntity $token, ScopeEntity $scope)
     {
-        $sql = 'INSERT INTO oauth_session_token_scopes'
+        $sql = 'INSERT INTO oauth_access_token_scopes'
              . ' (access_token, scope)'
              . ' VALUES'
              . ' (?, ?);';
@@ -75,7 +75,7 @@ class AccessTokenStorage extends StorageAdapter implements AccessTokenInterface
 
     public function delete(AbstractTokenEntity $token)
     {
-        $sql = 'DELETE FROM oauth_session_token_scopes'
+        $sql = 'DELETE FROM oauth_access_token_scopes'
              . ' WHERE access_token = ?;';
 
         $this->db->execute($sql, [$token->getId()]);
--- src/OAuth2/SessionStorage.php
+++ src/OAuth2/SessionStorage.php
@@ -17,9 +17,9 @@ class SessionStorage extends StorageAdapter implements SessionInterface
              . ' oauth_sessions.owner_id, oauth_sessions.client_id,'
              . ' oauth_sessions.client_redirect_uri'
              . ' FROM oauth_sessions'
-             . ' INNER JOIN oauth_session_access_tokens'
-             . ' ON oauth_session_access_tokens.session_id = oauth_sessions.id'
-             . ' WHERE oauth_session_access_tokens.access_token = ?;';
+             . ' INNER JOIN oauth_access_tokens'
+             . ' ON oauth_access_tokens.session_id = oauth_sessions.id'
+             . ' WHERE oauth_access_tokens.access_token = ?;';
 
         $results = $this->db->fetch($sql, [$access_token->getId()]);
 
@@ -41,9 +41,9 @@ class SessionStorage extends StorageAdapter implements SessionInterface
              . ' oauth_sessions.owner_id, oauth_sessions.client_id,'
              . ' oauth_sessions.client_redirect_uri'
              . ' FROM oauth_sessions'
-             . ' INNER JOIN oauth_authcodes'
-             . ' ON oauth_auth_codes.session_id = oauth_sessions.id'
-             . ' WHERE oauth_auth_codes.auth_code = ?;';
+             . ' INNER JOIN oauth_authorization_codes'
+             . ' ON oauth_authorization_codes.session_id = oauth_sessions.id'
+             . ' WHERE oauth_authorization_codes.authorization_code = ?;';
 
         $results = $this->db->fetch($sql, [$auth_code->getId()]);
 
@@ -63,10 +63,10 @@ class SessionStorage extends StorageAdapter implements SessionInterface
     {
         $sql = 'SELECT oauth_sessions.*'
              . ' FROM oauth_sessions'
-             . ' INNER JOIN oauth_session_token_scopes'
-             . ' ON oauth_sessions.id = oauth_session_token_scopes.session_access_token_id'
+             . ' INNER JOIN oauth_access_token_scopes'
+             . ' ON oauth_sessions.id = oauth_access_token_scopes.access_token_id'
              . ' INNER JOIN oauth_scopes'
-             . ' ON oauth_scopes.id = oauth_session_token_scopes.scope_id'
+             . ' ON oauth_scopes.id = oauth_access_token_scopes.scope_id'
              . ' WHERE oauth_sessions.id = ?;';
 
         $results = $this->db->fetch($sql, [$session->getId()]);
@@ -95,7 +95,7 @@ class SessionStorage extends StorageAdapter implements SessionInterface
 
     public function associateScope(SessionEntity $session, ScopeEntity $scope)
     {
-        $sql = 'INSERT INTO oauth_session_token_scopes'
+        $sql = 'INSERT INTO oauth_access_token_scopes'
              . ' (session_access_token_id, scope_id)'
              . ' VALUES'
              . ' (?, ?);';
